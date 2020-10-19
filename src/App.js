06cc489b084808da2a096a6a230c7862297c9b09
Akshay Deshpande <akshay.v.deshpande@accenture.com>
2020-10-19 22:19:47 +0530

    Changes in App.js for Opening Articles via article number

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
-import NewsCards from './components/NewsCards/NewsCards'
+import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers';
 
-const { ALAN_KEY_ID } = process.env;
+const { REACT_APP_ALAN_KEY_ID } = process.env;
 // import useStyles from './styles';
-const alanKey = ALAN_KEY_ID;
+const alanKey = process.env.REACT_APP_ALAN_KEY_ID;
 const App = ()=>{
 
     //const classes = useStyles();
@@ -14,21 +15,34 @@ const App = ()=>{
     useEffect(()=>{
         alanBtn({
             key: alanKey,
-            onCommand:({command,articles})=>{
+            onCommand:({command,articles,number})=>{
                 if(command === 'newHeadlines'){
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if(command === 'highlight'){
                     setActiveArticle((prevActiveArticle)=>prevActiveArticle+1);
+                } else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number,{fuzzy:true}):number;
+                    const article =  articles[parsedNumber-1]; 
+                    if(parsedNumber>20){
+                        alanBtn().playText('Please try that again');
+                    }else if(article) {
+                        window.open(article.url,'_blank');
+                        alanBtn().playText('Opening...');
+                    }
+                   
                 }
+
             }
         })
     },[])
+    
     return(
         <div>
             {/* <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" /> */}
             <h1>Alan AI News!</h1>
             <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+            
         </div>
     )
 }
